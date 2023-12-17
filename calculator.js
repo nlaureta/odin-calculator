05479bdd1a7b8d957a4498bbe9654daeceb894ae
nlaureta <73408370+nlaureta@users.noreply.github.com>
2023-12-16 20:31:32 -0800

    functioning number and operation buttons (in progress) +, - , * , / , = functional but does not use PEMDAS yet operations can be spammed but will soon be limted to one use

--- calculator.js
+++ calculator.js
@@ -12,33 +12,39 @@ function getNumbers() {
             buttonChoice.push(button.id);
 
             number = Number(buttonChoice.join(''));
-            
+            //console.log(typeof(number));
+
+            //console.log('returned');
+            //console.log(int);
             screenText.textContent = number;
-           
+            //console.log(buttonChoice);
+            //screenText.textContent += buttonChoice;
+            //return buttonChoice;
 
         });
-        
-        
+
+
     });
     buttonChoice = [];
-   
+    //console.log(number);
+    // return number;
 }
 
 function getOperations() {
     let operationChoice = "";
     const operationButton = document.querySelectorAll('.operations button');
     let screenText = document.querySelector('.screen');
-    
+
     operationButton.forEach((button) => {
         button.addEventListener("mouseup", () => {
-            
+
             operationChoice = button.id;
             operation = operationChoice;
-            if(numberStack.length - 1 <= operationStack.length - 1) { 
+            if (numberStack.length - 1 <= operationStack.length - 1) { // should be -1
                 numberStack.push(number);
             }
-          
-            if(numberStack[numberStack.length-1] != 0 && numberStack.length - 1 > operationStack.length - 2){
+            //number =0;
+            if (numberStack[numberStack.length - 1] != 0 && numberStack.length - 1 > operationStack.length - 2) {
                 operationStack.push(operation);
             }
             console.log(number);
@@ -46,47 +52,77 @@ function getOperations() {
             console.log(operationStack);
             console.log(numberStack);
             buttonChoice = [];
-          
+            //console.log(buttonChoice);
             screenText.textContent += operationChoice;
-            
-            if(operationChoice === '+' || operationChoice === '-' || operationChoice === '*' || operationChoice === '/' || operationChoice === '=') {
-                solve();
+            //return buttonChoice;
+            //operationChoice = [];
+            if (operationChoice === '+' || operationChoice === '-' || operationChoice === '*' || operationChoice === '/' || operationChoice === '=') {
+                //solve();
+                console.log("numberstack length: " + numberStack.length);
+                if (operationChoice === '=' && numberStack.length - 1 > 0) {
+                    console.log("from =" + total);
+                    //operationStack.pop();
+                    solve();
+                }
             }
 
         })
     });
-    
+
     //return operationButton;
 }
 
 function solve() {
-    
+
     let temp = 0;
-    
+    let operationPop = 0;
+    let nextNumber = 0;
     console.log("Num Stack: " + numberStack);
     console.log("Operation: " + operation);
+    total = numberStack[0];
     //console.log("popped: " + num[num.length - 1]);
-    
-    if (operationStack[operationStack.length - 1] === '+') {
-        console.log("adding");
-        // numberStack.forEach(number => {
-        //     total += number;
-        // })
-        total += numberStack[numberStack.length-1];
+    for (let i = 0; i < numberStack.length; i++) {
+        nextNumber = numberStack[i + 1];
+        console.log("popped operation: " + operationPop);
+        if (operationStack[i] === '+') {
+            console.log("adding");
+            total += nextNumber;
+            //console.log("numberstack[i]: "+numberStack[i]);
+            //console.log("numberStack[i+1]: "+numberStack[i+1]);
+            //console.log("total: "+total);
+        } else if (operationStack[i] === '-') {
+            console.log("subtracting");
+            total -= nextNumber;
+        } else if (operationStack[i] === '*') {
+            total *= nextNumber;
+        } else if (operationStack[i] === '/') {
+            total /= nextNumber;
+        } else if (operationStack[i] === '=') {
+            operationStack.pop();
+        }else {
+            console.log("Calculator broke. Please refresh");
+        }
+
         
-       
-    }else if (operationStack[operationStack.length - 1] === '-') {
-        console.log("subtracting");
-        // numberStack.forEach(number => {
-        //     total -= (number);
-        // })
-        total -= numberStack[numberStack.length-1];
-       
     }
-   
+
+    saveTotal();
+    console.log("-------after solve---------");
+    console.log(operationStack);
+    console.log(numberStack);
     console.log("total: " + total);
-    
+
     //return total;
+}
+
+function saveTotal() {
+    numberStack = [total];
+    operationStack = [];
+}
+
+function clearStacks() {
+    numberStack = [];
+    operationStack = [];
 }
 
 getNumbers();
